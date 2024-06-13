5b8aa68095d3f111faa46502b76585a1987b3a78
Hineill <hineill.cespedes@pucp.edu.pe>
2024-06-13 15:53:16 -0500

    Holaaa

--- src/main/java/com/example/labpokemon/Controller/HomeController.java
+++ src/main/java/com/example/labpokemon/Controller/HomeController.java
@@ -3,11 +3,17 @@ package com.example.labpokemon.Controller;
 import com.example.labpokemon.Daos.PokemonDao;
 import com.example.labpokemon.Repository.PokemonRepository;
 import com.example.labpokemon.Repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 @Controller
@@ -22,14 +28,36 @@ public class HomeController {
         this.pokemonRepository = pokemonRepository;
         this.userRepository = userRepository;
     }
-    //Pregunta 1
+    //Pregunta 1 Encontrar lugares de los pokemones
+    //https://pokeapi.co/api/v2/pokemon/{id or name}/encounters
     @GetMapping(value="/locationArea")
     public Object getPokemonLocationArea(@RequestParam("pokemon") String pokemon){
         LinkedHashMap<String , Object>  response =  new LinkedHashMap<String,Object>();
-
-
+        try {
+            if (! ((ArrayList<String>)(pokemonDao.buscarLugaresPokemones(pokemon))).isEmpty()){
+                ArrayList<String> listaLugares = (ArrayList<String>) (pokemonDao.buscarLugaresPokemones(pokemon));
+                ArrayList<LinkedHashMap<String,Object>> data = new ArrayList<>();
+                for(String nombreLugar:listaLugares){
+                    LinkedHashMap<String,Object>informacionLugar=new LinkedHashMap<>();
+                    informacionLugar.put("nombre",nombreLugar);
+                    data.add(informacionLugar);
+                }
+                response.put("Contenido" , data);
+                return ResponseEntity.ok(response);
+            }else{
+                System.out.println("No se encontro el pokemon");
+                LinkedHashMap<String, Object> resp = new LinkedHashMap<String, Object>();
+                resp.put("Error", "No se encontro el pokemon");
+                resp.put("Date", "" + LocalDateTime.now());
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resp);
+            }
+        }catch(Exception err) {
+            LinkedHashMap<String, Object> resp = new LinkedHashMap<String, Object>();
+            resp.put("Error", "ocurrio un error inesperado");
+            resp.put("Date", "" + LocalDateTime.now());
+            return ResponseEntity.badRequest().body(resp);
+        }
     }
-
     //Pregunta 2
 
     //Pregunta 3
--- src/main/java/com/example/labpokemon/Daos/PokemonDao.java
+++ src/main/java/com/example/labpokemon/Daos/PokemonDao.java
@@ -4,14 +4,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class PokemonDao {
-    public Object buscarDatosPorDNI(String pokemon ){
+    public Object buscarLugaresPokemones(String pokemon ){
         RestTemplate restTemplate = new RestTemplate();
-
+        //Usamos LinkedHashMap
+        //LinkedHashMap<String, Object> data = new LinkedHashMap<String, Object>();
+        //No sabemos la forma que tiene
         ResponseEntity<Object> response = restTemplate.getForEntity(
-                "https://api.apis.net.pe/v2/reniec/dni?numero="+dni+"&token=apis-token-8891.nCKic6UVmhldqck9sTCRzFbh2wGdR-dI", PersonaDni.class);
-        return response.getBody();
+                "https://pokeapi.co/api/v2/pokemon/"+pokemon+"/encounters", Object.class);
+        List<String> locations=new ArrayList<>();
+        ArrayList<LinkedHashMap<String, Object>> lista =  (ArrayList<LinkedHashMap<String, Object>>)response.getBody();
+        ArrayList<String> locationList = new ArrayList<String>();
+        for(LinkedHashMap<String,Object> map: lista){
+            LinkedHashMap<String,Object> aux = ((LinkedHashMap<String,Object>) map.get("location_area"));
+            locationList.add(""+ aux.get("name"));
+        }
+
+        return locationList;
     }
 
 }
